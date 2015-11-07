e669b237530d838ea71d46b97615704ef910ab65
kewang <cpckewang@gmail.com>
2015-11-08 00:20:14 +0800

    Test retrieve

--- parser.js
+++ parser.js
@@ -1,16 +1,12 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+  verbose: true,
+  logLevel: "debug"
+});
+var SESSION_URL = "http://acade.niu.edu.tw/NIU/outside.aspx?mainPage=LwBBAHAAcABsAGkAYwBhAHQAaQBvAG4ALwBUAEsARQAvAFAAUgBHAC8AUABSAEcAMQAxADAAMABfADAAMQAuAGEAcwBwAHgAPwBhAHkAZQBhAHIAcwBtAHMAPQAxADAANAAxAA==";
 var URL = "https://acade.niu.edu.tw/NIU/Application/TKE/PRG/PRG1100_01.aspx?ayearsms=1041";
 
-casper.start(URL, function(){
-  console.log("Loaded");
-
-  this.click("#QUERY_BTN1");
-});
-
-casper.then(function(){
+casper.start(SESSION_URL).thenOpen(URL).thenClick("#QUERY_BTN1", function(){
   this.echo(this.getHTML());
-
-  console.log("Clicked");
 });
 
 casper.run();
