60fe9aff5f7916d8bc2eb9cebc646b39d7ef5b48
kewang <cpckewang@gmail.com>
2015-11-08 14:31:04 +0800

    Complete retrieve course query results

--- parser.js
+++ parser.js
@@ -5,8 +5,16 @@ var casper = require('casper').create({
 var SESSION_URL = "http://acade.niu.edu.tw/NIU/outside.aspx?mainPage=LwBBAHAAcABsAGkAYwBhAHQAaQBvAG4ALwBUAEsARQAvAFAAUgBHAC8AUABSAEcAMQAxADAAMABfADAAMQAuAGEAcwBwAHgAPwBhAHkAZQBhAHIAcwBtAHMAPQAxADAANAAxAA==";
 var URL = "https://acade.niu.edu.tw/NIU/Application/TKE/PRG/PRG1100_01.aspx?ayearsms=1041";
 
-casper.start(SESSION_URL).thenOpen(URL).thenClick("#QUERY_BTN1", function(){
-  this.echo(this.getHTML());
+casper.start(SESSION_URL).thenOpen(URL, function(){
+  this.log("Open course page success");
+
+  this.click("#QUERY_BTN1");
 });
+
+casper.waitForSelector("#DataGrid", function(){
+  this.log("Wait for query results success");
+
+  this.echo(this.getHTML());
+}, null, 10000);
 
 casper.run();
